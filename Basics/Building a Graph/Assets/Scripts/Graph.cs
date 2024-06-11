47157b6cee003cfd0253f4e12b11c09a24feab72
Jade Voeller <vulpetite@icloud.com>
2024-06-11 13:21:52 +0200

    Update "Graph.cs" to animate

--- Basics/Building a Graph/Assets/Scripts/Graph.cs
+++ Basics/Building a Graph/Assets/Scripts/Graph.cs
@@ -7,18 +7,31 @@ public class Graph : MonoBehaviour {
     [SerializeField, Range(10, 100)]
     int resoution = 10;
 
+    Transform[] points;
+
     void Awake () {
         float step = 2f / resoution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        for (int i = 0; i < resoution; i++) { 
-            Transform point = Instantiate(pointPrefab);
+        points = new Transform[resoution];
+
+        for (int i = 0; i < points.Length; i++) { 
+            Transform point = points[i] = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
 
+        }
+    }
+
+    void Update() {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
         }
     }
 }
