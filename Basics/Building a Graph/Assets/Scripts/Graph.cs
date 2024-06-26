eaa307724887bf1b0570f66fe135244742bd87b7
Jade Voeller <vulpetite@icloud.com>
2024-06-26 13:43:43 +0200

    Update scripts to transition functions smoothly

--- Basics/Building a Graph/Assets/Scripts/Graph.cs
+++ Basics/Building a Graph/Assets/Scripts/Graph.cs
@@ -10,12 +10,21 @@ public class Graph : MonoBehaviour {
     [SerializeField]
     FunctionLibrary.FunctionName function;
 
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode;
+
     [SerializeField, Min(0f)]
-    float functionDuration = 1f;
+    float functionDuration = 1f, transitionDuration = 1f;
 
     Transform[] points;
 
     float duration;
+
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
 
     void Awake () {
         float step = 2f / resolution;
@@ -32,12 +41,31 @@ public class Graph : MonoBehaviour {
 
     void Update() {
         duration += Time.deltaTime;
-        if (duration >= functionDuration) {
-            duration -= functionDuration;
-            function = FunctionLibrary.GetNextFunctionName(function);
+        if (transitioning) {
+            if (duration >= transitionDuration) {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
         }
-        UpdateFunction();
+        else if (duration >= functionDuration) {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+        if (transitioning) {
+            UpdateFunctionTransition();
+        } else {
+            UpdateFunction();
+        }
     }
+
+    void PickNextFunction() {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+
 
     void UpdateFunction () {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
@@ -53,6 +81,27 @@ public class Graph : MonoBehaviour {
             }
             float u = (x + 0.5f) * step - 1f;
             points[i].localPosition = f(u, v, time);
+        }
+    }
+
+    void UpdateFunctionTransition() {
+        FunctionLibrary.Function
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(
+                u, v, time, from, to, progress
+            );
         }
     }
 }
