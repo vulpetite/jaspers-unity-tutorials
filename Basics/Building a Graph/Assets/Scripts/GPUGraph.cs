a20d69fae1ac2e55f3b447c07b4d5ff79ccf5f66
Jade Voeller <vulpetite@icloud.com>
2024-07-03 15:21:43 +0200

    Create "FunctionLibrary.compute" shader

--- Basics/Building a Graph/Assets/Scripts/GPUGraph.cs
+++ Basics/Building a Graph/Assets/Scripts/GPUGraph.cs
@@ -6,6 +6,9 @@ public class GPUGraph : MonoBehaviour {
     int resolution = 100;
 
     [SerializeField]
+    ComputeShader computeShader;
+
+    [SerializeField]
     FunctionLibrary.FunctionName function;
 
     public enum TransitionMode { Cycle, Random }
@@ -23,6 +26,24 @@ public class GPUGraph : MonoBehaviour {
     FunctionLibrary.FunctionName transitionFunction;
 
     ComputeBuffer positionsBuffer;
+
+    static readonly int
+        positionsId = Shader.PropertyToID("_Positions"),
+        resolutionId = Shader.PropertyToID("_Resolution"),
+        stepId = Shader.PropertyToID("_Step"),
+        timeId = Shader.PropertyToID("_Time");
+
+    void UpdateFunctionOnGPU() {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+    }
 
     void OnEnable() {
         positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
@@ -46,6 +67,8 @@ public class GPUGraph : MonoBehaviour {
             transitionFunction = function;
             PickNextFunction();
         }
+
+        UpdateFunctionOnGPU();
     }
 
     void PickNextFunction() {
